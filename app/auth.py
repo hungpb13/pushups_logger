c448d43fe45811d5091485bf810c75e53c475280
hungpb13 <hungpb13@gmail.com>
2025-05-27 00:39:35 +0700

    Python Flask Tutorial - Handling GET and POST Requests

--- app/auth.py
+++ app/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, url_for, redirect
 
 auth = Blueprint("auth", __name__)
 
@@ -7,10 +7,28 @@ def signup():
     return render_template("signup.html")
 
 
+@auth.route("/signup", methods=['POST'])
+def signup_post():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    print(name, email, password)
+
+    return redirect(url_for('auth.login'))
+
 @auth.route("/login")
 def login():
     return render_template("login.html")
 
+@auth.route("/login", methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    print(email, password)
+
+    return redirect(url_for('main.profile'))
 
 @auth.route("/logout")
 def logout():
