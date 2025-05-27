7d471fed3a9ee7387de52be69fb0eb8f48dff3b3
hungpb13 <hungpb13@gmail.com>
2025-05-27 12:51:00 +0700

    Python Flask Tutorial - Building Flask CRUD Application | Create and Read Operations

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from .models import Workout, User
+from . import db
+from flask import Blueprint, render_template, redirect, url_for, request
 from flask_login import login_required, current_user
 
 main = Blueprint("main", __name__)
@@ -12,3 +14,32 @@ def index():
 @login_required
 def profile():
     return render_template("profile.html", name=current_user.name)
+
+
+@main.route("/new_workout")
+@login_required
+def new_workout():
+    return render_template("new_workout.html")
+
+
+@main.route("/new_workout", methods=['POST'])
+@login_required
+def new_workout_post():
+    pushups = request.form.get('pushups')
+    comment = request.form.get('comment')
+
+    workout = Workout(pushups=pushups, comment=comment, author=current_user)
+
+    db.session.add(workout)
+    db.session.commit()
+
+    return redirect(url_for('main.all_workouts'))
+
+
+@main.route("/all_workouts")
+@login_required
+def all_workouts():
+    user = User.query.filter_by(email=current_user.email).first_or_404()
+    workouts = user.workouts
+
+    return render_template("all_workouts.html", workouts=workouts, user=user)
